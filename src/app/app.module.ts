f5743311226c39db205adc2c7472c378ac908564
Lucas de Araujo Lima <lucas.dearaujo@castgroup.com.br>
2022-06-09 11:16:09 -0300

    Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarroselComponent } from './carrosel/carrosel.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CarroselComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
